fdc24ff6ff93a877421a1add92acbc03b0e85bb4
Ibrahiem <ibrahiem.mohammed96@gmail.com>
2015-09-10 22:37:36 -0500

    modify assembly codec file

--- CS261/Homework2.c
+++ CS261/Homework2.c
@@ -6,8 +6,6 @@
  *Net-ID: imoham3
 */
 
-<<<<<<< HEAD
-=======
  /*1) It took 13 assembly instructions for my progrm to complete
    2) The index variable is located at 0x000000000040053e in rbp. It is later accessed at 0x0000000000400556.
    3) The string is stored at 0x000000000040054c in eax
@@ -18,20 +16,9 @@
    the other registers are shown in the image in the zip file along with the assembly code
    */
 
-<<<<<<< HEAD
->>>>>>> 77073a3e898a09c0d2a50f367278232d3b0c01aa
 int main(){
 	int i;
 	for (i = 0; i <= 25; i++){
 		printf("\nSeptember is hot in Chicago this year");
 	}
-<<<<<<< HEAD
-=======
-=======
-		int i=0;
-		for (i = 0; i <= 25; i++){
-				printf("\nSeptember is hot in Chicago this year");
-		}
->>>>>>> 63232d760d4975a121e3c040849bcd7436b5c973
->>>>>>> 77073a3e898a09c0d2a50f367278232d3b0c01aa
 }
