5dd25d647f7f8c4abed4255b637f91f96e666b61
Ibrahiem Mohammad <ibrahiem.mohammed96@gmail.com>
2015-09-15 16:20:55 -0500

    add Project 2 cs211

---
+++ CS211/Project2-first.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Assignment: Project 2
+  Name: Ibrahiem Mohammad
+  net-ID: imoham3
+  Due Date: 9/17/15
+*/
+
+typedef struct stack_struct
+{
+	char* darr;
+	int size; //size of array
+	int top; //specifies top of stack
+} stack;
+
+int stk_init(){
+
+}
+
+int stk_check_if_empty(){
+
+}
+
+void stk_push(){
+
+}
+
+void stk_pop(){
+
+}
+
+void stk_get_top(){
+
+}
+
+void stk_reset(){
+
+}
+
+
+int main(){
+
+	//get user input using fgets-each line is less than 300 characters, but array can be of infinite size (that's why we are growing the array)
+	//if input contains only q/Q quit program
+	//
+}
