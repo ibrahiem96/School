8c27b7c26667893e10d87c672f6464233a710b69
Ibrahiem Mohammad <ibrahiem.mohammed96@gmail.com>
2015-09-15 23:55:44 -0500

    modify hwk3

--- CS261/hwk3practice.c
+++ CS261/hwk3practice.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
-int main(){
-
-	int i = 0;
-	i++;
-	
-	i*=2;
-
-	return i;
-
-}
+int main()
+{
+ long x = 0x00001101;
+ //long result;
+ long mask;
+ for (mask = 0; mask < x ; mask = >> 3) {
+ //  result |= ________;
+ }
+ //printf("result %lx\n",result);
+ }
