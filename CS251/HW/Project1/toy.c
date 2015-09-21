5604672d4a5b475cfe39b3f8f34a0012e83828f6
Ibrahiem <ibrahiem.mohammed96@gmail.com>
2015-09-21 10:38:07 -0500

    make changes to 251 hw

--- CS251/HW/Project1/toy.c
+++ CS251/HW/Project1/toy.c
@@ -1,12 +1,15 @@
 
 #include "sset.h"
-
+/*This is a test file for sset.c*/
 int main(){
 SSET *s;
 
   int a[] = {2, 8, 1, 3, 1, 8, 0, 4};
 
   s = sset_from_array(a, 8);
+
+  sset_free(s);
+  sset_display(s);
 
 }
 
