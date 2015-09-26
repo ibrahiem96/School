95d0c16ded24c51e199b5f2772675c29ab1bff9a
Ibrahiem Mohammad <ibrahiem.mohammed96@gmail.com>
2015-09-26 13:36:28 -0500

    make change to hwk4 files and add hwk4 zip file

--- CS261/fib.s
+++ CS261/fib.s
@@ -1,18 +1,9 @@
-	.file	"fib.c"
-	.text
-	.globl	fib
-	.type	fib, @function
+
 fib:
-.LFB0:
-	.cfi_startproc
 	pushl	%ebp
-	.cfi_def_cfa_offset 8
-	.cfi_offset 5, -8
 	movl	%esp, %ebp
-	.cfi_def_cfa_register 5
 	pushl	%ebx
 	subl	$20, %esp
-	.cfi_offset 3, -12
 	cmpl	$1, 8(%ebp)
 	jg	.L2
 	movl	$1, %eax
@@ -31,28 +22,12 @@ fib:
 .L3:
 	addl	$20, %esp
 	popl	%ebx
-	.cfi_restore 3
 	popl	%ebp
-	.cfi_restore 5
-	.cfi_def_cfa 4, 4
 	ret
-	.cfi_endproc
-.LFE0:
-	.size	fib, .-fib
-	.section	.rodata
-.LC0:
-	.string	"Fib of %d is %d\n"
-	.text
-	.globl	main
-	.type	main, @function
+	
 main:
-.LFB1:
-	.cfi_startproc
-	pushl	%ebp
-	.cfi_def_cfa_offset 8
-	.cfi_offset 5, -8
+
 	movl	%esp, %ebp
-	.cfi_def_cfa_register 5
 	andl	$-16, %esp
 	subl	$32, %esp
 	movl	$5, 28(%esp)
@@ -65,11 +40,3 @@ main:
 	movl	$.LC0, (%esp)
 	call	printf
 	leave
-	.cfi_restore 5
-	.cfi_def_cfa 4, 4
-	ret
-	.cfi_endproc
-.LFE1:
-	.size	main, .-main
-	.ident	"GCC: (Ubuntu/Linaro 4.7.3-1ubuntu1) 4.7.3"
-	.section	.note.GNU-stack,"",@progbits
