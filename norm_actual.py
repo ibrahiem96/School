d0e2ac8e7192237298f0652730034eb2d44361fb
ibrahiem96 <ibrahiem.mohammed96@gmail.com>
2016-04-26 16:57:35 -0500

    Update norm_actual.py

--- norm_actual.py
+++ norm_actual.py
@@ -6,10 +6,10 @@ import requests
 
 if __name__ == '__main__':
 
-    resp = requests.get("http://files.vladstudio.com/joy/where_tahrs_live/wall/vladstudio_where_tahrs_live_2880x1800_signed.jpg")
+    r_handler = requests.get("http://files.vladstudio.com/joy/where_tahrs_live/wall/vladstudio_where_tahrs_live_2880x1800_signed.jpg")
     
-    if resp.status_code == 200:
+    if r_handler.status_code == 200:
 
-        print(resp.headers["Content-Length"])
-        with open("test_normal.jpg", "wb") as f:
-            f.write(resp.content)
+        print(r_handler.headers["Content-Length"])
+        with open("test_normal.jpg", "wb") as file:
+            file.write(r_handler.content)
